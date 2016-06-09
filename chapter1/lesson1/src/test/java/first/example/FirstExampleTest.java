5c067f0e63e4543a704f558cfc7d70c9819441aa
Robin Péricé <r0perice@enib.fr>
2016-06-09 22:55:33 +0200

    learn move tutorial to website repo. Code stay here

--- chapter1/lesson1/src/test/java/first/example/FirstExampleTest.java
+++ chapter1/lesson1/src/test/java/first/example/FirstExampleTest.java
@@ -16,55 +16,55 @@ import org.testng.annotations.Test;
  */
 public class FirstExampleTest {
 
-	/** The consumer. */
-	private FirstExampleConsumer consumer;
+    /** The consumer. */
+    private FirstExampleConsumer consumer;
 
-	/** The producer. */
-	private FirstExampleProducer producer;
+    /** The producer. */
+    private FirstExampleProducer producer;
 
-	/**
-	 * Prepare test.
-	 */
-	@BeforeClass
-	public void prepareTest() {
+    /**
+     * Prepare test.
+     */
+    @BeforeClass
+    public void prepareTest() {
 
-		/** The consumer properties. */
-		final Properties producerProperties = new Properties();
-		producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		producerProperties.put(ProducerConfig.CLIENT_ID_CONFIG, "FirstExampleProducer");
-		producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-				"org.apache.kafka.common.serialization.IntegerSerializer");
-		producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-				"org.apache.kafka.common.serialization.StringSerializer");
+        /** The consumer properties. */
+        final Properties producerProperties = new Properties();
+        producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.17.0.1:9092");
+        producerProperties.put(ProducerConfig.CLIENT_ID_CONFIG, "FirstExampleProducer");
+        producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.IntegerSerializer");
+        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringSerializer");
 
-		producer = new FirstExampleProducer(producerProperties);
+        producer = new FirstExampleProducer(producerProperties);
 
-		/** The consumer properties. */
-		final Properties consumerProperties = new Properties();
-		consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "FirstExampleConsumer");
-		consumerProperties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-		consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-		consumerProperties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
-		consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-				"org.apache.kafka.common.serialization.IntegerDeserializer");
-		consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-				"org.apache.kafka.common.serialization.StringDeserializer");
+        /** The consumer properties. */
+        final Properties consumerProperties = new Properties();
+        consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.17.0.1:9092");
+        consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "FirstExampleConsumer");
+        consumerProperties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        consumerProperties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.IntegerDeserializer");
+        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                "org.apache.kafka.common.serialization.StringDeserializer");
 
-		consumer = new FirstExampleConsumer(consumerProperties);
+        consumer = new FirstExampleConsumer(consumerProperties);
 
-	}
+    }
 
-	@Test(enabled = true)
-	private void pushToKafka() {
-		String[] messages = { "a", "b", "c", "d", "e" };
-		for (String letter : messages) {
-			producer.push("topic", letter);
-		}
-	}
+    @Test(enabled = true, dependsOnMethods = { "pushToKafka" })
+    private void pullFromKafka() {
+        consumer.pull("topic");
+    }
 
-	@Test(enabled = true, dependsOnMethods = { "pushToKafka" })
-	private void pullFromKafka() {
-		consumer.pull("topic");
-	}
+    @Test(enabled = true)
+    private void pushToKafka() {
+        String[] messages = { "a", "b", "c", "d", "e" };
+        for (String letter : messages) {
+            producer.push("topic", letter);
+        }
+    }
 }
